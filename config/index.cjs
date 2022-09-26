4d2eaf4aed1d2b75c8eddca0762eff3b894e20b1
heanci <109217233+heanci@users.noreply.github.com>
2022-09-26 13:55:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouxQ26vaAvOfP3RJI6D4Gd_DfkEI',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'RnMCcmsYH82j4x9JiWNwiuqqG7VHQHhJtKe57QdZcyA',
+      useTemplateId: '-UL2QzZ-lITTprRu9lSnzefu4DTnINY1pb-9Ebkl58o',
       // 所在省份
       province: '广东',
       // 所在城市
